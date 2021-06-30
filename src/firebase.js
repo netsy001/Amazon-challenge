3206da805693d9354beae919bcdff66e316976b7
Surendra Choudary Nettam <nettem.surendra@gmail.com>
2021-07-01 07:20:19 +0930

    initial commit , header component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDPEQQTwpO7pZzu7Pa57ziK8bVifYrKDtU",
+    authDomain: "myshop-d68d9.firebaseapp.com",
+    projectId: "myshop-d68d9",
+    storageBucket: "myshop-d68d9.appspot.com",
+    messagingSenderId: "71886357447",
+    appId: "1:71886357447:web:05239e362cb8d413e8cfe2",
+    measurementId: "G-NESN54FKQE"
+  };
