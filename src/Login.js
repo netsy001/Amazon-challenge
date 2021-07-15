6496149f059e9adbbcf9840423ed2ecd4ee856e1
Surendra Choudary Nettam <nettem.surendra@gmail.com>
2021-07-15 21:17:13 +0930

    adding onClick functionalites to signIn and register buttons and preventDefault functionality to prevent the refreshing of the page

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css"
 import myshopLogo from './assets/My-shop.png';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+    }
+
+    const register = e => {
+        e.preventDefault();
+
+    }
+
     return (
+
         <div className="login">
             <Link to="/">
                 <img className="login__logo" src={myshopLogo} alt="myshopLogo" />
@@ -12,14 +26,19 @@ const Login = () => {
             <div className="login__container">
                 <h1>Sign-in</h1>
                 <form>
+
                     <h5>E-mail</h5>
-                    <input type="text" />
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+
                     <h5>Password</h5>
-                    <input type="password" />
-                    <button className="login__signInButton">Sign in</button>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                    <button onClick={signIn} className="login__signInButton">Sign in</button>
+
                 </form>
+
                 <p>By sigining-in you agree to my🛒shop's Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.</p>
-                <button className="login__registerButton">Create your my🛒shop Account </button>
+                <button onClick={register} className="login__registerButton">Create your my🛒shop Account </button>
+
             </div>
         </div>
 
